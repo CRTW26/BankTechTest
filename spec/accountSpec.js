3918af4b0945183529d3e2e4d4bdc6eee9e77129
Connor Worthington <worthington.connor@gmail.com>
2020-11-03 08:34:18 +0000

    Refactor: accountSpec
    Adds beforeEach function to create a new account before each test is carried out

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,12 +1,14 @@
 describe('Account', function() {
 
-  it('has a balance of 0.00', function() {
+  beforeEach(function() {
     account = new Account;
+  });
+
+  it('has a balance of 0.00', function() {
     expect(account.showBalance()).toEqual(0.00);
   });
 
   it('can deposit into account', function() {
-    account = new Account;
     account.deposit(10.00);
     expect(account.showBalance()).toEqual(10.00);
   });
